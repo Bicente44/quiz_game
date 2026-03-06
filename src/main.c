b62d9f5201d26dbc9341690ea98b506da594f2c1
Vincent Welbourne <vincent.vw04@gmail.com>
2026-03-06 14:26:26 -0500

    Feat: implement user input and validation
    Reading user input as string and converting to integer ensuring no
    setting characters to int error or out of bounds. Made as a callable
    main function rather than bloating main.
    
    In a rush to work but..
    TODO: finish some more of main, but mainly implement way of clearing buffer
    after character/number is read.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,50 @@
 #include "qg_work.h"
 
+int option_input() {
+  char str_option[3];
+  int int_option = -1;
+  printf("Please enter a valid integer between 1 - 4:\n>");
+  if (fgets(str_option, sizeof(str_option), stdin) != NULL) {
+    if ((int_option = atoi(str_option)) >= 1 && int_option <= 4) {
+      return int_option;
+    } else {
+      printf("Number is not witin 1-4 try again\n");
+      return -1;
+    }
+  } else {
+    printf("input read failure, NULL error\n");
+    return -1;
+  }
+}
+
 int main() {
-  printf("Welcome to the 'Quiz Game'!\n");
+  bool loop = true;
+  int option = -1;
+
+  while (loop) {
+    if ((option = option_input()) <= 0)
+      continue;
+
+    switch (option) {
+      /* Start Game */
+    case 1:
+      break;
+      /* Edit questions */
+    case 2:
+      /* check if not admin, if true then print cant do that and continue */
+      break;
+      /* Help */
+    case 3:
+      break;
+      /* Exit */
+    case 4:
+      printf("Exiting the program...");
+      loop = false;
+      break;
+    default:
+      break;
+    }
+  }
+
   return 0;
 }
--- src/qg_work.h
+++ src/qg_work.h
@@ -1,1 +1,4 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef enum { false, true } bool;
