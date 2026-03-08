fb7194f075e75f3db9fd99080194c804838826ba
Vincent Welbourne <vincent.vw04@gmail.com>
2026-03-08 14:11:15 -0400

    Feat: Added get number of questions and print questions functions
    Added these utility functions to help clean out the qg_work and to allow
    reusablility.
    
    Print is more to help debug and see q list contents

--- src/qg_utils.c
+++ src/qg_utils.c
@@ -1,4 +1,5 @@
 #include "qg_utils.h"
+#include "qg_work.h"
 
 int option_input(int n) { // 'n' is number of questions
   char str_option[3];
@@ -14,5 +15,42 @@ int option_input(int n) { // 'n' is number of questions
   } else {
     printf("input read failure, NULL error\n");
     return -1;
+  }
+}
+
+int get_n_questions(int difficulty) {
+  int n_questions = 0;
+  switch (difficulty) {
+  case 1:
+    n_questions = N_EASY_QUESTIONS;
+    break;
+  case 2:
+    n_questions = N_MED_QUESTIONS;
+    break;
+  case 3:
+    n_questions = N_HARD_QUESTIONS;
+    break;
+  }
+  return n_questions;
+}
+
+void shuffle_questions() {}
+int game_clean() { return 0; }
+
+/* DEBUG ? */
+
+void print_list_details(int n_questions, int difficulty,
+                        Question *question_list) {
+
+  printf("Difficulty: %d. Number of questions: %d\n", difficulty, n_questions);
+  for (int i = 0; i < n_questions; i++) {
+    printf("Question %d: %s\n", i + 1, question_list[i].question);
+    printf("  Correct Answer: %s\n", question_list[i].correct_a);
+
+    printf("  Incorrect Answers:\n");
+    for (int j = 0; j < 3; j++) {
+      printf("    %d. %s\n", j + 1, question_list[i].incorrect_a[j]);
+    }
+    printf("\n");
   }
 }
--- src/qg_utils.h
+++ src/qg_utils.h
@@ -1,4 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int option_input(int n);
+
+int get_n_questions(int difficulty);
+
+void shuffle_questions();
+
+int game_clean();
